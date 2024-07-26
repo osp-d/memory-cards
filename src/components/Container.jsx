e8869f01768eb3aa23789483c1b361f985ebfc0a
osp-d <edudamirosp@gmail.com>
2024-07-26 15:21:56 +0500

    Add cards data fetching and cards rendering

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,57 @@
+import { useState, useEffect } from 'react';
 import Card from './Card';
 export default Container;
 
 function Container({ setScore }) {
+  const [cards, setCards] = useState([{ name: 'initial' }]);
+
+  useEffect(() => {
+    let data = fetch('https://pokeapi.co/api/v2/pokemon?limit=12', {
+      mode: 'cors',
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        return response.results;
+      });
+
+    async function getData(data) {
+      try {
+        let result = await data;
+        result = await Promise.all(
+          result.map(async (card) => {
+            const image = fetch(card.url, { mode: 'cors' })
+              .then((response) => response.json())
+              .then((response) => {
+                return response.sprites.front_default;
+              });
+
+            card.image = await image;
+            return card;
+          })
+        );
+
+        setCards(result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getData(data);
+  }, []);
+
   return (
-    <div>
-      <Card setScore={setScore} />
+    <div className="container">
+      {cards.map((card) => {
+        console.log(card);
+        return (
+          <Card
+            setScore={setScore}
+            key={card.name}
+            name={card.name}
+            image={card.image}
+          />
+        );
+      })}
     </div>
   );
 }
